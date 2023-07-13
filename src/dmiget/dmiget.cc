46db753e47d557057294596fddaba02efd23c475
Perry Werneck <perry.werneck@gmail.com>
2023-07-12 22:52:38 -0300

    Refactoring argument parser.

--- src/dmiget/dmiget.cc
+++ src/dmiget/dmiget.cc
@@ -22,11 +22,141 @@
  #include <dmiget/table.h>
  #include <iostream>
  #include <getopt.h>
+ #include <functional>
+ #include <cstring>
 
  using namespace std;
  namespace DMI = DMIget;
 
+ static string input_file;
+ static string delimiter{"\t"};
+
+ static DMI::Table * TableFactory() {
+	if(input_file.empty())
+		return new DMI::Table();
+	return new DMI::Table(input_file.c_str());
+ }
+
  int main(int argc, char **argv) {
+
+	static const struct Worker {
+		char short_arg;
+		const char *long_arg;
+		const bool required;
+		const char *help;
+		const std::function<void(const char *argument)> method;
+
+		Worker(char s, const char *l, const char *h, const bool r, const std::function<void(const char *argument)> m)
+			: short_arg{s}, long_arg{l}, required{r}, help{h}, method{m} {
+		}
+	} workers[] {
+
+		{
+			'a',"all","",false,[](const char *) {
+
+				auto table = TableFactory();
+
+				table->for_each([](shared_ptr<DMI::Value> value){
+					cout << value->url() << delimiter << value << endl;
+					return true;
+				});
+
+				delete table;
+
+			}
+		},
+		{
+			'i',"urls","",false,[](const char *) {
+
+				auto table = TableFactory();
+
+				table->for_each([](shared_ptr<DMI::Value> value){
+					cout << value->url() << endl;
+					return true;
+				});
+
+				delete table;
+
+			}
+		},
+		{
+			'V',"values","",true,[](const char *filename) {
+
+				auto table = TableFactory();
+
+				table->for_each([](shared_ptr<DMI::Value> value){
+					cout << value << endl;
+					return true;
+				});
+
+				delete table;
+
+			}
+		},
+		{
+			'N',"names","",true,[](const char *filename) {
+
+				auto table = TableFactory();
+
+				table->for_each([](shared_ptr<DMI::Value> value){
+					cout << value->name() << endl;
+					return true;
+				});
+
+				delete table;
+
+			}
+		},
+		{
+			'I',"input-file","",true,[](const char *filename) {
+				input_file = filename;
+			}
+		},
+		{
+			'D',"delimiter","",true,[](const char *d) {
+				delimiter = d;
+			}
+		},
+	};
+
+	while(--argc) {
+
+		bool found = false;
+		const char *argument = *(++argv);
+
+		if(!strncmp(argument,"--",2)) {
+
+			argument += 2;
+			const char *value = strchr(argument,'=');
+			string name;
+			if(value) {
+				name.assign(argument,value-argument);
+				value++;
+			} else {
+				name.assign(argument);
+				value = "";
+			}
+
+			for(const Worker &worker : workers) {
+
+				found = (strcmp(name.c_str(),worker.long_arg) == 0);
+				if(found) {
+					worker.method(value);
+					break;
+				}
+
+			}
+
+		}
+
+		if(!found) {
+			cerr << "Invalid argument" << endl;
+			return -1;
+		}
+
+	}
+
+	/*
 
 	/// @brief Command line arguments.
 	static struct option options[] = {
@@ -139,6 +269,8 @@
 		cerr << endl << e.what() << endl;
 		return -1;
 	}
+
+	*/
 
 	return 0;
 
